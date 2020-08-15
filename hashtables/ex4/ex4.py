2d2409074c18a6a11d22b6cfe35d2dd783285e40
Sophia Sagan <sophiabraddock82@gmail>
2020-08-14 20:39:49 -0400

    ex4 passing

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,24 @@
+import math
+
 def has_negatives(a):
     """
     YOUR CODE HERE
     """
     # Your code here
+    ht = {} # hash table for numbers and count
+    result = [] # list of results
+
+    # loop through the array
+    for num in a:
+        num = abs(num) # convert all numbers to positive
+        if num in ht: # if number in hash table 
+            ht[num] += 1 # add to count
+            if ht[num] > 1: # if hash table count
+                result.append(num) # number to results
+                # print(ht)
+                # print(result)
+        else:
+            ht[num] = 1 # add to hash table
 
     return result
 
